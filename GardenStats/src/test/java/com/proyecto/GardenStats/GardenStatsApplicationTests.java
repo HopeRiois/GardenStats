439b0fdb3dee672aff72709fe412ab26e7a7e568
Alvaro Rios <73720485+HopeRiois@users.noreply.github.com>
2021-10-01 17:16:05 -0500

    try

---
+++ GardenStats/src/test/java/com/proyecto/GardenStats/GardenStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.GardenStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

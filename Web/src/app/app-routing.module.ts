652a5488ac6e14b310d2d5e7cf6f2a9ff9c9a0a3
Cristian Serrano <cristiangserrano@gmail.com>
2021-10-01 21:06:53 -0500

    trabajo

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidenavComponent } from './button/sidenav/sidenav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sidenav', component:SidenavComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
